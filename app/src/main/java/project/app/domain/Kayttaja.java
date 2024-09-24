1c59536aaba3d35af17fcfded3431ea6b472a328
Ilona <ilona.hiltunen@myy.haaga-helia.fi>
2024-09-24 15:58:42 +0300

    Käyttäjä entity ja repository luotu

---
+++ app/src/main/java/project/app/domain/Kayttaja.java
@@ -0,0 +1,99 @@
+package project.app.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Kayttaja {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long kayttaja_id;
+    
+    private String etunimi;
+
+    private String sukunimi;
+
+    private String salasana;
+
+    private String kayttajatunnus;
+
+    private String oikeus;
+
+    //Tyhjä konstruktori
+    public Kayttaja() {
+    }
+
+    //Konstruktori arvoilla
+    public Kayttaja(long kayttaja_id, String etunimi, String sukunimi, String salasana, String kayttajatunnus,
+            String oikeus) {
+        this.kayttaja_id = kayttaja_id;
+        this.etunimi = etunimi;
+        this.sukunimi = sukunimi;
+        this.salasana = salasana;
+        this.kayttajatunnus = kayttajatunnus;
+        this.oikeus = oikeus;
+    }
+
+    //Getterit ja setterit
+    public long getKayttaja_id() {
+        return kayttaja_id;
+    }
+
+    public void setKayttaja_id(long kayttaja_id) {
+        this.kayttaja_id = kayttaja_id;
+    }
+
+    public String getEtunimi() {
+        return etunimi;
+    }
+
+    public void setEtunimi(String etunimi) {
+        this.etunimi = etunimi;
+    }
+
+    public String getSukunimi() {
+        return sukunimi;
+    }
+
+    public void setSukunimi(String sukunimi) {
+        this.sukunimi = sukunimi;
+    }
+
+    public String getSalasana() {
+        return salasana;
+    }
+
+    public void setSalasana(String salasana) {
+        this.salasana = salasana;
+    }
+
+    public String getKayttajatunnus() {
+        return kayttajatunnus;
+    }
+
+    public void setKayttajatunnus(String kayttajatunnus) {
+        this.kayttajatunnus = kayttajatunnus;
+    }
+
+    public String getOikeus() {
+        return oikeus;
+    }
+
+    public void setOikeus(String oikeus) {
+        this.oikeus = oikeus;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "Kayttaja [kayttaja_id=" + kayttaja_id + ", etunimi=" + etunimi + ", sukunimi=" + sukunimi
+                + ", salasana=" + salasana + ", kayttajatunnus=" + kayttajatunnus + ", oikeus=" + oikeus + "]";
+    }
+
+    
+
+    
+}
---
+++ app/src/main/java/project/app/domain/KayttajaRepository.java
@@ -0,0 +1,7 @@
+package project.app.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KayttajaRepository extends CrudRepository<Kayttaja, Long> {
+
+}
