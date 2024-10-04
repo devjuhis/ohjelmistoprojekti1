9a5f5594223be7b21017ba68277f92e890c84139
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-10-04 13:38:04 +0300

    lisätty endpoint maksutapahtumaan

--- app/src/main/java/project/app/domain/MaksutapahtumaRepository.java
+++ app/src/main/java/project/app/domain/MaksutapahtumaRepository.java
@@ -1,7 +1,6 @@
 package project.app.domain;
 
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
 
 
 public interface MaksutapahtumaRepository extends CrudRepository<Maksutapahtuma, Long> {
--- app/src/main/java/project/app/web/RestMaksuController.java
+++ app/src/main/java/project/app/web/RestMaksuController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -59,5 +61,12 @@ public class RestMaksuController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+     // REST luodaan uusi maksutapahtuma
+    @PostMapping("/maksutapahtumat")
+    public Maksutapahtuma createMaksutapahtuma(@RequestBody Maksutapahtuma maksutapahtuma) {
+        logger.info("Creating new maksutapahtuma");
+        return maksurepository.save(maksutapahtuma);
     }
 }
