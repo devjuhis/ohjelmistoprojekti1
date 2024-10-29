f30c45df8cfa48d2812d29932e7890e9ea0f7816
sanni-bhd725 <sanni.nevala@myy.haaga-helia.fi>
2024-10-29 18:10:23 +0200

    Poistettu ylimääräinen import

--- app/src/main/java/project/app/web/RestTapahtumaController.java
+++ app/src/main/java/project/app/web/RestTapahtumaController.java
@@ -16,7 +16,6 @@ import project.app.domain.Tapahtuma;
 import project.app.domain.TapahtumaRepository;
 
 import java.time.LocalDate;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
