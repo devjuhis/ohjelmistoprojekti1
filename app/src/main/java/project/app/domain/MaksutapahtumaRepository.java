15b1a76f9ff9d38ede7e17b652a65a8e79aead96
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-11-17 16:18:25 +0200

    Lisätty maksutapahtumarepoon findByRemovedFalse()

--- app/src/main/java/project/app/domain/MaksutapahtumaRepository.java
+++ app/src/main/java/project/app/domain/MaksutapahtumaRepository.java
@@ -1,8 +1,11 @@
 package project.app.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface MaksutapahtumaRepository extends CrudRepository<Maksutapahtuma, Long> {
     Maksutapahtuma findByMaksutapahtumaId(long maksutapahtumaid);
+    List<Maksutapahtuma> findByRemovedFalse();
 }
