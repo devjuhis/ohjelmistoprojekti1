587c8414d65002b7a798c2bfc107efef8e8909aa
Eriikakl <eriika.klenberg@myy.haaga-helia.fi>
2024-11-18 13:32:17 +0200

    Lisätty Aktiivinen profiili h2 testeihin

--- app/src/test/java/project/app/domain/TapahtumaTests.java
+++ app/src/test/java/project/app/domain/TapahtumaTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("h2")
 public class TapahtumaTests {
 
     @Autowired
